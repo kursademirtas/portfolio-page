f18042520ce3a3c18d0829f815800891db1d20d3
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-03-11 14:07:59 +0300

    animation added.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const homePage = document.querySelector('.home');
 const aboutPage = document.querySelector('.aboutPage');
 const aboutButton = document.querySelector('.about');
 const backButton = document.querySelector('.back');
-
+const welcomeSection = document.querySelector('#welcome-section');
 function goHomePage() {
 	aboutPage.style.display = "none";
 	homePage.style.display = "block";
@@ -12,7 +12,29 @@ function goAboutPage() {
 	homePage.style.display = "none";
 	aboutPage.style.display = "block";
 }
-
+function loadAnimation() {
+	welcomeSection.classList.remove('welcomeLoad');
+	welcomeSection.classList.add('welcome')
+}
 aboutButton.addEventListener('click', goAboutPage);
 backButton.addEventListener('click', goHomePage);
+window.addEventListener('load', loadAnimation)
+
+gsap.registerPlugin(ScrollTrigger);
+	let	sections = gsap.utils.toArray(".work"),
+		tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".works",
+				pin: true,
+				marker:true,
+				scrub: 1,
+        		snap: 1 / (sections.length),
+				start: "top top",
+				end: "+=5000"
+			}
+		});
+tl.to(sections, {
+  xPercent: -100 * (sections.length),
+  ease: "none"
+});
 
